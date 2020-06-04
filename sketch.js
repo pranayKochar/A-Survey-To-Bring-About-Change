04d63c958552b921547a81ea8bfb213e2dc0ad0c
pranayKochar <64725229+pranayKochar@users.noreply.github.com>
2020-06-04 12:22:14 +0530

    Added Files

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas, backgroundImage;
+
+
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player;
+
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+
+ 
+}
+
+
+function draw(){
+  
+}
